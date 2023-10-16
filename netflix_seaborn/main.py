0b5eb238d0284047445c73ede994fb889e5dfc83
KASSAS20 <kassas20@mail.ru>
2023-10-16 06:41:24 +0400

    task_4

--- netflix_seaborn/main.py
+++ netflix_seaborn/main.py
@@ -8,19 +8,49 @@ data = pd.read_csv("Netflix.csv", encoding='latin1')
 # plt.show()
 
 class task_2:
-    def __init__(self, data):
+    def __init__(self, data) -> None:
         self.data = data
     
-    def data_type(self):
+    def data_type(self) -> dict:
         result = {}
         for name in self.data.columns:
             result[name] = str(data[name].dtype)
         return result
     
-    def data_count(self):
+    def data_count(self) -> tuple:
         return f'{self.data.shape[1],self.data.shape[0]}'
 
 
+class task_4:
+    def __init__(self, data) -> None:
+        self.data = data
+    
+    def destribution_genre(self) -> None:
+        genre_counts = self.data.groupby('Genre')['Title'].count().reset_index()
+        sns.barplot(data=genre_counts, x='Genre', y='Title')
+        plt.xlabel('Genre')
+        plt.ylabel('Title')
+        plt.title('Распределение фильмов по жанрам')
+        plt.show()
+    
+    def destribution_lang(self):
+        lang_counts = self.data.groupby('Language')['Title'].count().reset_index()
+        sns.barplot(data=lang_counts, x='Language', y='Title')
+        plt.xlabel('Language')
+        plt.ylabel('Title')
+        plt.title('Распределение фильмов по языкам')
+        plt.show()
+
+    def destribution_year(self):
+        premiere_counts = self.data.groupby('Premiere')['Title'].count().reset_index()
+        sns.barplot(data=premiere_counts, x='Premiere', y='Title')
+        plt.xlabel('Premiere')
+        plt.ylabel('Title')
+        plt.title('Распределение фильмов по годам')
+        plt.show()
+
+        
 
 
-# print(task_1(data=data).data_count())
+
+
