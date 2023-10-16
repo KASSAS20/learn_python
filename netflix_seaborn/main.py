5c6f46ceaeb29fdd9dd56fd512ece7c5aee7924b
KASSAS20 <kassas20@mail.ru>
2023-10-16 04:50:14 +0400

    task_1

--- netflix_seaborn/main.py
+++ netflix_seaborn/main.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv("Netflix.csv", encoding='latin1')
-data.head()
-sns.scatterplot(data=data, x='Runtime', y='IMDB Score')
-plt.show()
-print(data)
+# data.head()
+# sns.barplot(data=data, x='Premiere', y='Runtime')
+# plt.show()
+
+class task_1:
+    def __init__(self, data):
+        self.data = data
+    
+    def data_type(self):
+        result = {}
+        for name in self.data.columns:
+            result[name] = str(data[name].dtype)
+        return result
+
+print(task_1(data=data).data_type())
