d26e2b68db4f714a1ed5a964d96e058290c687ce
KASSAS20 <kassas20@mail.ru>
2023-10-18 02:02:41 +0400

    fix create_journal

--- journal_server/create_journal.py
+++ journal_server/create_journal.py
@@ -8,23 +8,34 @@ class work_of_path:
 
     def create_journal(self) -> None:
         fake = Faker()
+        
+        lines = list()
         http_status = [200, 200, 200, 200, 200, 200, 200,
                         200, 200, 200, 200, 200, 200, 200,
                         200, 200, 200, 200, 200, 200, 201,
                         204, 400, 401, 403, 404, 500, 502, 503]
-        lines = list()
+        list_url = list()
+        list_date = list()
+
+        for _ in range(20):
+            list_url.append(fake.url())
+            for _ in range(3):
+                list_date.append(fake.date_this_century())
+                
         with open(self.path, 'w') as journal:
             for _ in range(1000):
                 ip = fake.ipv4()
-                date = fake.date_this_century()
+                date = choice(list_date)
                 time = fake.time()
-                url = fake.url()
+                url = choice(list_url)
                 status_server = choice(http_status)
                 size_response = randint(5, 10_240)
                 journal.write(f'{ip}|{date}|{time}|{url}|{status_server}|{size_response}\n')
+        
         with open(self.path, 'r+') as journal:
             for line in journal:
                 lines.append(line)
+
             count = 0
             while count != -1:
                 for i in range(999):
@@ -35,6 +46,7 @@ class work_of_path:
                         lines[i] = lines[i+1]
                         lines[i+1] = save
                         count+=1
+
                 if count == 0:
                     count = -1
                 else:
@@ -48,3 +60,5 @@ class work_of_path:
 
 def make(name):
     work_of_path(f'{name}.txt').create_journal()
+
+make('journal')
